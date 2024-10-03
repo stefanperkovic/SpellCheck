a057d1b3abe6157fbecac4f55b51da366a2ad3a7
stefanperkovic <stefan.perkovic@menloschool.org>
2024-10-02 18:26:55 -0700

    Finished creating the node class and also finished the insert function in the trie.

--- src/Node.java
+++ src/Node.java
@@ -3,28 +3,21 @@ public class Node {
     private boolean isWord;
     private Node[] next;
 
+    public Node(){
+        this.isWord = false;
+        this.next = new Node[26];
+    }
 
     public boolean isWord() {
-
-
+        return isWord;
     }
 
     public void setWord(){
-
-
-
-
-
+        isWord = true;
     }
-
 
     public Node[] getNext(){
-
-
-
+        return this.next;
     }
-
-
-
 
 }
--- src/Trie.java
+++ src/Trie.java
@@ -3,10 +3,22 @@ public class Trie {
     private Node root;
 
 
+    public Trie (){
+        root = new Node();
+    }
+
+
     public void insert(String s){
+        Node current = root;
+        for (int i = 0; i < s.length(); i++){
+            int index  = s.charAt(i) - 'a';
+            if (root.getNext()[index] == null){
+                root.getNext()[index] = new Node();
 
-
-
+            }
+            current = current.getNext()[index];
+        }
+        current.setWord();
     }
 
     public boolean lookup(String s){
